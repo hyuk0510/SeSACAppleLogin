d312c69c20ba32a939b6997cbc1a772c62fad738
Hyuk0510 <hyouk970510@gmail.com>
2023-12-28 10:11:04 +0900

    Initial Commit

---
+++ SeSACAppleLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACAppleLogin
+//
+//  Created by 선상혁 on 2023/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
